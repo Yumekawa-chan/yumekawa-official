dd2a715b716cdc6819924abd4f69e68d3877e62e
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2025-01-01 17:37:25 +0900

    update: dotフォントを導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,21 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { JSX } from 'react';
+import localFont from 'next/font/local';
 
 export const metadata: Metadata = {
   title: 'Yumekawa Holdings',
 };
+
+const dotGothic16 = localFont({
+  src: [
+    {
+      path: '../../public/DotGothic16-Regular.ttf',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-dotgothic16',
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +24,7 @@ export default function RootLayout({
 }>): JSX.Element {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body className={`${dotGothic16.className}`}>{children}</body>
     </html>
   );
 }
